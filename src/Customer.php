ecb945467a091f5ea740f6158e616af4f9da823a
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-10-29 20:52:37 +0700

    add title

--- src/Customer.php
+++ src/Customer.php
@@ -8,8 +8,8 @@ class Customer
     {
     }
 
-    public function sayHello(string $name = "Guest"): string
+    public function sayHello(string $name = "Guest", $title = ""): string
     {
-        return "Hello $name, My Name is $this->name";
+        return "Hello $title $name, My Name is $this->name";
     }
 }
